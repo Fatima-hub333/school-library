e208eccb73c07801374f968afef7691febb335ed
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2022-07-26 15:59:27 +0500

    fix linter errors

--- student.rb
+++ student.rb
@@ -1,6 +1,6 @@
 require_relative 'person'
 
-class Student < person
+class Student < Person
   attr_accessor :classroom
 
   def initialize(classroom, age, name = 'Unknown', parent_permission: true)
@@ -12,7 +12,7 @@ class Student < person
     @classroom = classroom
     classroom.students.push(self) unless classroom.students.include?(self)
   end
-  
+
   def play_hooky
     "¯\(ツ)/¯"
   end
