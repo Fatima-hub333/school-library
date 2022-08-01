bc54624b80fab1f688e188f7f1fc4e550a8002fc
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2022-08-01 22:22:10 +0500

    Update book_options.rb

--- book_options.rb
+++ book_options.rb
@@ -6,4 +6,23 @@ class BookOptions
   def initialize
     @books = []
   end
+
+  def list_all_books
+    if @books.count.zero?
+       puts "There is no book yet...\n"
+    end
+  end
+
+  def create_book
+    print 'Title: '
+    title = gets.chomp
+
+    print 'Author: '
+    author = gets.chomp
+
+    book = Book.new(title, author)
+    @books << book
+
+    puts 'Book added successfully'
+  end
 end
