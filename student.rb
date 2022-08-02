3e86cbf468ef8e645ffe012ed9ab7ad92da49b2e
jr-cast <castanos@pm.me>
2022-08-02 13:35:29 -0500

    Delete unnecessary comment line

--- student.rb
+++ student.rb
@@ -6,7 +6,6 @@ class Student < Person
 
   def initialize(age, name = 'Unknown', parent_permission: 'true')
     super(age, name, parent_permission)
-    # @classroom = classroom
   end
 
   def owner=(classroom)
