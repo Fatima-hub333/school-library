2c8f0285195618ab5811ffc0374a4fb376ce8737
jr-cast <castanos@pm.me>
2022-08-04 10:43:39 -0500

    Add tests to book class

--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -1,14 +1,21 @@
 require "book"
-require "rental"
 
 describe Book do
   context "To instantiate a book" do
+    Title = 'Fight Club'
+    Author = 'Palaniuk'
+    let(:book) { Book.new(Title, Author)}
 
-    it "can add the title and author to the book" do
-      Title = 'title'
-      Author = 'author'
-      book = Book.new(Title, Author)
-      expect(book.title).to eq('title')
+    it "can add the title to the book" do
+      expect(book.title).to eq('Fight Club')
+    end
+
+    it "can add author to the book" do
+      expect(book.author).to eq('Palaniuk')
+    end
+
+    it "when book created there must not be rentals" do
+      expect(book.rentals.length == 0).to be_truthy
     end
   end
 end
